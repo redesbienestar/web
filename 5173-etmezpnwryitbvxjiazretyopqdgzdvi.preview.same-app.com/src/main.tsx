667d05445cc5c70d9fc406ec4402fd12775ab190
redesbienestar <proteccionbienestar@gmail.com>
2025-06-26 19:00:45 -0300

    Add files via upload

---
+++ 5173-etmezpnwryitbvxjiazretyopqdgzdvi.preview.same-app.com/src/main.tsx
@@ -0,0 +1,15 @@
+import { jsxDEV } from "/node_modules/bippy/dist/jsx-dev-runtime.js?v=42b6b716";
+import __vite__cjsImport1_reactDom_client from "/node_modules/.vite/deps/react-dom_client.js?v=a0eb1593"; const createRoot = __vite__cjsImport1_reactDom_client["createRoot"];
+import "/src/index.css";
+import App from "/src/App.tsx";
+const rootElement = document.getElementById("root");
+if (!rootElement) {
+  throw new Error("Failed to find root element");
+}
+createRoot(rootElement).render(/* @__PURE__ */ jsxDEV(App, {}, void 0, false, {
+  fileName: "/home/project/redes-bienestar-clone/src/main.tsx",
+  lineNumber: 10,
+  columnNumber: 32
+}, this));
+
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJtYXBwaW5ncyI6IkFBUytCO0FBVC9CLFNBQVNBLGtCQUFrQjtBQUMzQixPQUFPO0FBQ1AsT0FBT0MsU0FBUztBQUVoQixNQUFNQyxjQUFjQyxTQUFTQyxlQUFlLE1BQU07QUFDbEQsSUFBSSxDQUFDRixhQUFhO0FBQ2hCLFFBQU0sSUFBSUcsTUFBTSw2QkFBNkI7QUFDL0M7QUFFQUwsV0FBV0UsV0FBVyxFQUFFSSxPQUFPLHVCQUFDLFNBQUQ7QUFBQTtBQUFBO0FBQUE7QUFBQSxPQUFJLENBQUciLCJuYW1lcyI6WyJjcmVhdGVSb290IiwiQXBwIiwicm9vdEVsZW1lbnQiLCJkb2N1bWVudCIsImdldEVsZW1lbnRCeUlkIiwiRXJyb3IiLCJyZW5kZXIiXSwiaWdub3JlTGlzdCI6W10sInNvdXJjZXMiOlsibWFpbi50c3giXSwic291cmNlc0NvbnRlbnQiOlsiaW1wb3J0IHsgY3JlYXRlUm9vdCB9IGZyb20gXCJyZWFjdC1kb20vY2xpZW50XCI7XG5pbXBvcnQgXCIuL2luZGV4LmNzc1wiO1xuaW1wb3J0IEFwcCBmcm9tIFwiLi9BcHAudHN4XCI7XG5cbmNvbnN0IHJvb3RFbGVtZW50ID0gZG9jdW1lbnQuZ2V0RWxlbWVudEJ5SWQoXCJyb290XCIpO1xuaWYgKCFyb290RWxlbWVudCkge1xuICB0aHJvdyBuZXcgRXJyb3IoXCJGYWlsZWQgdG8gZmluZCByb290IGVsZW1lbnRcIik7XG59XG5cbmNyZWF0ZVJvb3Qocm9vdEVsZW1lbnQpLnJlbmRlcig8QXBwIC8+KTtcbiJdLCJmaWxlIjoiL2hvbWUvcHJvamVjdC9yZWRlcy1iaWVuZXN0YXItY2xvbmUvc3JjL21haW4udHN4In0=
